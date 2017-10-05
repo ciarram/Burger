fe3d7fd00f12f0fdc0d7bbf273d44dd4972064ee
Ciarra Murphy <mciarra@ymail.com>
2017-10-05 15:23:46 -0400

    linked main handlebar to server

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,10 @@
 var path = require("path");
 
 module.exports = function(app){
-    app.get("/", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/test.html"));
+    // app.get("/", function(req, res){
+    //     res.sendFile(path.join(__dirname, "../views/layouts/main.handlebars"));
+    // });
+    app.get('*', function (req, res) {
+        res.render('index');
     });
 };
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var express_handlebars = require('express-handlebars');
 var app = express();
-
+var exphbs = require('express-handlebars');
 var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
  require("./routes/apiRoutes")(app);
  require("./routes/htmlRoutes")(app);
